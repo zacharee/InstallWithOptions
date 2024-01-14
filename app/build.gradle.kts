ea61fc1afe19d204a1b062bb31b502892c3d12bd
Zachary Wander <zachary.wander@gmail.com>
2024-01-13 23:17:43 -0500

    Version 0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
