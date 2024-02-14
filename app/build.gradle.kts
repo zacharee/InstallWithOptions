190b89c3eb0182166055f3b57647c9e3ea7e97cc
Zachary Wander <zachary.wander@gmail.com>
2024-02-14 18:16:36 -0500

    Update Compose Compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         aidl = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
+        kotlinCompilerExtensionVersion = "1.5.9"
     }
     packaging {
         resources {
