1d59029d57935748128dc8b4c0a77868b6bac4bd
Zachary Wander <zachary.wander@gmail.com>
2024-03-14 14:52:37 -0400

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         aidl = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.9"
+        kotlinCompilerExtensionVersion = "1.5.10"
     }
     packaging {
         resources {
