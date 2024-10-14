829d6ce0dffe84a20d8f278bb1316cb227a9afb8
Zachary Wander <zachary.wander@gmail.com>
2024-10-14 16:32:24 -0400

    Exclude dependency metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,12 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    dependenciesInfo {
+        // Disables dependency metadata when building APKs.
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles.
+        includeInBundle = false
+    }
 }
 
 dependencies {
