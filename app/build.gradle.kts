9b6c94406cac5c2f7927d69a77e96e45f8ef12e1
Zachary Wander <zachary.wander@gmail.com>
2026-01-31 11:18:52 -0500

    Version 0.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.zwander.installwithoptions"
         minSdk = 24
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.9.0"
+        versionCode = 26
+        versionName = "0.9.1"
 
         vectorDrawables {
             useSupportLibrary = true
