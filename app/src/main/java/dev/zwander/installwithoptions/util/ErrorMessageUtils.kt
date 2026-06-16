3d356af2f4eff00905f0b082bd215d518754f475
Zachary Wander <zachary.wander@gmail.com>
2026-06-16 18:27:30 -0400

    Add specific error message for INSTALL_FAILED_ALREADY_EXISTS

--- app/src/main/java/dev/zwander/installwithoptions/util/ErrorMessageUtils.kt
+++ app/src/main/java/dev/zwander/installwithoptions/util/ErrorMessageUtils.kt
@@ -4,9 +4,13 @@ import android.content.Context
 import dev.zwander.installwithoptions.R
 
 fun Context.getSpecificErrorMessage(legacyStatus: Int): String? {
-    return when (legacyStatus) {
+    val res = when (legacyStatus) {
+        // INSTALL_FAILED_ALREADY_EXISTS
+        -1 -> R.string.install_failed_already_exists
         // INSTALL_FAILED_NO_MATCHING_ABIS
-        -113 -> resources.getString(R.string.install_failed_no_matching_abis)
+        -113 -> R.string.install_failed_no_matching_abis
         else -> null
     }
+
+    return res?.let { resources.getString(it) }
 }
